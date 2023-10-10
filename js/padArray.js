ca032351a464d52a8676a64447c64271c2e58972
Jkim1122 <jkimz1122@gmail.com>
2023-10-09 20:23:53 -0500

    array.length-pad

--- js/padArray.js
+++ js/padArray.js
@@ -1,6 +1,22 @@
 // REMEMBER TO PSEUDOCODE
 const pad = (array, minSize, value=null) => {
-    // code goes here : ) 
+    
+    if (array.length > minSize){
+        return array
+    }
+    while (array.length != minSize){
+        array.push(value);
+        // array = array.push(value)
+        // return array
+    }
+    return array
 }
 
 exports.pad = pad;
+
+// console.log(pad(my_array, 0))
+console.log(pad([1, 2, 3], 10))
+console.log(pad([1, 2, 3], 5, 'apple'))
+
+// console.log(pad([1, 2, 3], 5, "apple"))
+//     output -> [1, 2, 3, 'apple', 'apple']
